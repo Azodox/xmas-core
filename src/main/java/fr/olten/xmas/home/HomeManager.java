85f01af50aa09b6e9ce87e717f4796dd5bcdd118
Azodox_ <likemoi99@gmail.com>
2022-01-24 22:24:40 +0100

    Update add method.

--- src/main/java/fr/olten/xmas/home/HomeManager.java
+++ src/main/java/fr/olten/xmas/home/HomeManager.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class HomeManager {
@@ -47,8 +48,23 @@ public class HomeManager {
         }
     }
 
-    public void add(Player player, Home home){
+    public boolean exists(Player player){
+        return this.getConfiguration(player) != null;
+    }
 
+    public void add(Player player, Home home){
+        //TODO : add logs
+        YamlConfiguration conf = getConfiguration(player);
+        List<String> homes = conf.getStringList("homes");
+        homes.add(home.name());
+        conf.set("homes", homes);
+
+        conf.set("homes." + home.name() + "x", home.location().getX());
+        conf.set("homes." + home.name() + "y", home.location().getY());
+        conf.set("homes." + home.name() + "z", home.location().getZ());
+        conf.set("homes." + home.name() + "world_name", home.location().getWorld().getName());
+        conf.set("homes." + home.name() + "yaw", home.location().getYaw());
+        conf.set("homes." + home.name() + "pitch", home.location().getPitch());
     }
 
     public YamlConfiguration getConfiguration(Player player){
