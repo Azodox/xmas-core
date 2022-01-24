91eafd4929cf40249dae9e13c7c5152d0f231d69
Azodox_ <likemoi99@gmail.com>
2022-01-24 22:25:22 +0100

    Try to create a new home for the player when he executes /home command.

--- src/main/java/fr/olten/xmas/commands/HomeCommand.java
+++ src/main/java/fr/olten/xmas/commands/HomeCommand.java
@@ -1,12 +1,22 @@
 package fr.olten.xmas.commands;
 
+import fr.olten.xmas.Core;
+import fr.olten.xmas.home.Home;
 import io.github.llewvallis.commandbuilder.CommandContext;
 import io.github.llewvallis.commandbuilder.ExecuteCommand;
 import io.github.llewvallis.commandbuilder.OptionalArg;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class HomeCommand {
+
+    private final Core core;
+
+    public HomeCommand(Core core) {
+        this.core = core;
+    }
 
     @ExecuteCommand
     private void home(CommandContext ctx, @OptionalArg OfflinePlayer offlinePlayer, String homeName){
@@ -17,7 +27,8 @@ public class HomeCommand {
         }else {
             if(ctx.getSender() instanceof Player){
                 Player player = (Player) ctx.getSender();
-
+                Home home = new Home(UUID.randomUUID().toString(), player.getLocation());
+                this.core.getHomeManager().add(player, home);
             }
         }
     }
