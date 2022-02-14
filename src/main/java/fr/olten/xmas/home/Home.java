11dadd8368621375f1994b72a9f7bdf4ca230bf8
Azodox_ <likemoi99@gmail.com>
2022-02-14 18:20:57 +0100

    Add toString method.

--- src/main/java/fr/olten/xmas/home/Home.java
+++ src/main/java/fr/olten/xmas/home/Home.java
@@ -3,4 +3,12 @@ package fr.olten.xmas.home;
 import org.bukkit.Location;
 
 public record Home(String name, Location location) {
+
+    @Override
+    public String toString() {
+        return "Home{" +
+                "name='" + name + '\'' +
+                ", location=" + location.toString() +
+                '}';
+    }
 }
