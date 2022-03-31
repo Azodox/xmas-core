ac0d1e9e30a19a0d2b75ea73bf7fc72f4f913cfb
Azodox_ <likemoi99@gmail.com>
2022-03-31 16:10:10 +0200

    Comment registerHeads method because it currently occurs an error (to fix). Add necessary lines to check and update what we need for TeamNameTagManager.java to work.

--- src/main/java/fr/olten/xmas/Core.java
+++ src/main/java/fr/olten/xmas/Core.java
@@ -7,15 +7,22 @@ import fr.olten.xmas.commands.SetHomeCommand;
 import fr.olten.xmas.commands.arguments.OfflinePlayerArgument;
 import fr.olten.xmas.home.HomeManager;
 import fr.olten.xmas.listener.InventoryClickListener;
+import fr.olten.xmas.listener.PlayerChatListener;
 import fr.olten.xmas.listener.PlayerJoinListener;
+import fr.olten.xmas.listener.RankChangedListener;
+import fr.olten.xmas.manager.TeamNameTagManager;
 import fr.olten.xmas.util.HeadUtil;
 import fr.olten.xmas.util.ScrollerInventory;
 import io.github.llewvallis.commandbuilder.CommandBuilder;
 import io.github.llewvallis.commandbuilder.DefaultInferenceProvider;
 import io.github.llewvallis.commandbuilder.ReflectionCommandCallback;
+import net.valneas.account.rank.RankUnit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 /**
     Welcome to 2022 xmas event's core.
@@ -24,6 +31,8 @@ import org.bukkit.plugin.java.JavaPlugin;
     such as homes, teleportation, automatic messages and all general functionnalities.
 */
 public class Core extends JavaPlugin {
+
+    public static final String CHAT_FORMAT = "%rank%" + "%1$s " + ChatColor.DARK_GRAY + "» " + ChatColor.RESET + "%2$s";
 
     private HeadUtil headUtil;
     private final HomeManager homeManager = new HomeManager();
@@ -38,7 +47,10 @@ public class Core extends JavaPlugin {
     public void onEnable() {
         this.homeManager.setup(this);
         this.headUtil = new HeadUtil();
-        
+
+        Arrays.stream(RankUnit.values()).forEach(TeamNameTagManager::init);
+        getServer().getOnlinePlayers().forEach(TeamNameTagManager::update);
+
         registerCommands();
         /*
             Register every single listener needed.
@@ -46,14 +58,22 @@ public class Core extends JavaPlugin {
         registerEvents(
                 new PlayerJoinListener(this),
                 new ScrollerInventory.ScrollerInventoryListener(),
-                new InventoryClickListener(this)
+                new InventoryClickListener(this),
+                new PlayerChatListener(this),
+                new RankChangedListener()
         );
         /*
             Register with HeadUtil.java all needed mc heads.
         */
-        registerHeads();
+        //registerHeads();
 
         getLogger().info("Enabled!");
+    }
+
+
+    @Override
+    public void onDisable() {
+        TeamNameTagManager.reset();
     }
 
     /**
@@ -65,7 +85,7 @@ public class Core extends JavaPlugin {
         }
     }
 
-    /*
+    /**
         Register commands.
         @link https://github.com/LlewVallis/command-builder
     */
@@ -81,6 +101,18 @@ public class Core extends JavaPlugin {
         this.headUtil.addHead("333ae8de7ed079e38d2c82dd42b74cfcbd94b3480348dbb5ecd93da8b81015e3", "nextPage");
         this.headUtil.addHead("81c96a5c3d13c3199183e1bc7f086f54ca2a6527126303ac8e25d63e16b64ccf", "previousPage");
         this.headUtil.addHead("d01afe973c5482fdc71e6aa10698833c79c437f21308ea9a1a095746ec274a0f", "information");
+
+        /* Bird houses */
+        this.headUtil.addHead("d68c95c89ebe1b5a89b9e51bb21a5cc7379bbf5b109d8b517713e772f28f379", "birdhouse-orange");
+        this.headUtil.addHead("123daf8c965e5034c4bfc79d2d6ee8339f3a08a7e62bc56b7ab8eb2d7d070", "birdhouse-magenta");
+        this.headUtil.addHead("b0f1afd3e65e622346691748b1d7a5bb39c4fd6b1ef28af5f338545b2571c", "birdhouse-brown");
+        this.headUtil.addHead("efe24c6f52ddc08359f792301eca96aecf66979dc899c43242d2587ebcb70e6", "birdhouse-lime");
+        this.headUtil.addHead("cdaea3e13a513da1051ea59d17edfb03e8d8388ede88888476dfb693f2c8398", "birdhouse-cyan");
+        this.headUtil.addHead("4e338bb32287afd632bc874b32f53a56f44b7aee26cd3d5c6fdad99f340f470", "birdhouse-purple");
+        this.headUtil.addHead("24fbe85787c12787ddceb852d7142dae907578a272cc2021555ba805d227ea7", "birdhouse-yellow");
+        this.headUtil.addHead("fc1bd7ce2d527ec6f76b8e81f19d69d1b9ff4d5b6cd6cc764eed1945a6c6c", "birdhouse-green");
+        this.headUtil.addHead("75465f77e7fd4217384998c33859ad9636e1893bee405dd2519423b51767", "birdhouse-red");
+        this.headUtil.addHead("75465f77e7fd4217384998c33859ad9636e1893bee405dd2519423b51767", "birdhouse-red");
     }
 
     public HomeManager getHomeManager() {
