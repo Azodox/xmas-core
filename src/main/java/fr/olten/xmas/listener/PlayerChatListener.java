8435bdd9002ea71c272f7a6cd917ac29e7d4f55b
Azodox_ <likemoi99@gmail.com>
2022-08-08 04:16:35 +0200

    Using Paper's AsyncChatEvent

--- src/main/java/fr/olten/xmas/listener/PlayerChatListener.java
+++ src/main/java/fr/olten/xmas/listener/PlayerChatListener.java
@@ -1,21 +1,26 @@
 package fr.olten.xmas.listener;
 
 import fr.olten.xmas.Core;
-import net.md_5.bungee.api.ChatColor;
-import net.valneas.account.AccountManager;
-import net.valneas.account.AccountSystem;
-import org.bukkit.Bukkit;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.valneas.account.AccountSystemApi;
+import net.valneas.account.rank.RankManager;
+import net.valneas.account.rank.RankUnit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatListener implements Listener {
 
     private final Core core;
+    private final AccountSystemApi accountSystem;
 
-    public PlayerChatListener(Core core) {
+    public PlayerChatListener(Core core, AccountSystemApi accountSystem) {
         this.core = core;
+        this.accountSystem = accountSystem;
     }
 
     /**
@@ -25,19 +30,20 @@ public class PlayerChatListener implements Listener {
      * @param event The event
      */
     @EventHandler(priority = EventPriority.HIGH)
-    public void onChat(AsyncPlayerChatEvent event) {
+    public void onChat(AsyncChatEvent event) {
         var player = event.getPlayer();
-        var accountSystem = (AccountSystem) Bukkit.getPluginManager().getPlugin("AccountSystem");
-
-        if(accountSystem == null){
-            core.getLogger().severe("AccountSystem is not installed");
-            return;
-        }
-
-        var accountManager = new AccountManager(accountSystem, player);
+        var accountManager = accountSystem.getAccountManager(player);
         var rank= accountManager.newRankManager().getMajorRank();
 
-        event.setMessage(event.getMessage().replace("<3", ChatColor.of("#C51104") + "\u2764"));
-        event.setFormat(Core.CHAT_FORMAT.replace("%rank%", rank.getPrefix()));
+        RankManager<? extends RankUnit> rankManager = accountManager.newRankManager();
+
+
+
+        event.message(event.message().replaceText(builder -> builder.matchLiteral("<3").replacement(Component.text("\u2764").color(TextColor.color(0xC51104)))));
+        event.renderer((source, sourceDisplayName, message, viewer) ->
+                MiniMessage.miniMessage().deserialize(rank.getPrefix())
+                        .append(sourceDisplayName.color(NamedTextColor.NAMES.value(rank.getColor())))
+                .append(Component.text(" : ").color(NamedTextColor.GRAY))
+                .append(message));
     }
 }
