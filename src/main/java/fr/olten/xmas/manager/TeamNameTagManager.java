d23a30c5f1a18c5e42e019772a4706b21cd13db8
Azodox_ <likemoi99@gmail.com>
2022-08-08 04:19:43 +0200

    Updating methods to fit with new account system's version

--- src/main/java/fr/olten/xmas/manager/TeamNameTagManager.java
+++ src/main/java/fr/olten/xmas/manager/TeamNameTagManager.java
@@ -1,7 +1,10 @@
 package fr.olten.xmas.manager;
 
+import com.google.common.base.Preconditions;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.valneas.account.AccountManager;
-import net.valneas.account.AccountSystem;
+import net.valneas.account.AccountSystemApi;
 import net.valneas.account.rank.RankUnit;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -23,9 +26,15 @@ public class TeamNameTagManager {
      * @param rank The rank to create the team for.
      */
     public static void init(RankUnit rank){
-        var team = SCOREBOARD.registerNewTeam(String.valueOf(rank.getPower()));
-        team.setPrefix(rank.getPrefix());
-        team.setColor(rank.getColor());
+        var teamName = Integer.toString(rank.getPower());
+        if(SCOREBOARD.getTeam(teamName) != null){
+            SCOREBOARD.getTeam(teamName).unregister();
+        }
+
+        var team = SCOREBOARD.registerNewTeam(teamName);
+        team.prefix(MiniMessage.miniMessage().deserialize(rank.getPrefix()));
+        team.suffix(MiniMessage.miniMessage().deserialize(rank.getSuffix()));
+        team.color(NamedTextColor.NAMES.value(rank.getColor()));
         team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
     }
 
@@ -43,8 +52,10 @@ public class TeamNameTagManager {
      * @param player The player to update.
      */
     public static void update(Player player){
-        var accountSystem = (AccountSystem) Bukkit.getPluginManager().getPlugin("AccountSystem");
-        var accountManager = new AccountManager(accountSystem, player);
+        var provider = Bukkit.getServicesManager().getRegistration(AccountSystemApi.class);
+        Preconditions.checkNotNull(provider, "AccountSystem is not registered");
+
+        var accountManager = provider.getProvider().getAccountManager(player);
         update(accountManager);
     }
 
@@ -52,7 +63,7 @@ public class TeamNameTagManager {
      * Update the team of a player.
      * @param accountManager The account manager of the player.
      */
-    public static void update(AccountManager accountManager){
+    public static void update(AccountManager<?, ?> accountManager){
         var rank = accountManager.newRankManager().getMajorRank();
         var team = SCOREBOARD.getTeam(String.valueOf(rank.getPower()));
         team.addEntry(accountManager.getName());
