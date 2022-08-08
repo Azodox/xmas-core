2437a41ba34bb8a2528172cc3c8e1c06de8b03b3
Azodox_ <likemoi99@gmail.com>
2022-08-08 04:12:29 +0200

    Using acf in the homes command

--- src/main/java/fr/olten/xmas/commands/HomesCommand.java
+++ src/main/java/fr/olten/xmas/commands/HomesCommand.java
@@ -1,19 +1,18 @@
 package fr.olten.xmas.commands;
 
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.*;
 import fr.olten.xmas.Core;
-import fr.olten.xmas.util.HeadUtil;
-import fr.olten.xmas.util.ItemBuilder;
-import fr.olten.xmas.util.ScrollerInventory;
-import io.github.llewvallis.commandbuilder.CommandContext;
-import io.github.llewvallis.commandbuilder.ExecuteCommand;
-import io.github.llewvallis.commandbuilder.PlayerOnlyCommand;
+import fr.olten.xmas.inventory.HomeListView;
+import me.saiintbrisson.minecraft.ViewFrame;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
-import java.util.Random;
-
-public class HomesCommand {
+@CommandAlias("homes")
+public class HomesCommand extends BaseCommand {
 
     private final Core core;
 
@@ -21,13 +20,29 @@ public class HomesCommand {
         this.core = core;
     }
 
-    @ExecuteCommand
-    @PlayerOnlyCommand
-    public void homes(CommandContext ctx, OfflinePlayer player){
-        var sender = (Player) ctx.getSender();
-        var homes = this.core.getHomeManager().getHomes(player.getUniqueId());
-        var list = HeadUtil.getTranslator().keySet().stream().filter(s -> s.startsWith("birdhouse-")).toList();
-        var items = homes.stream().map(home -> new ItemBuilder(HeadUtil.getHead(list.get(new Random().nextInt(list.size()))).clone()).setName(ChatColor.GOLD + home.name()).build()).toList();
-        new ScrollerInventory(items, ChatColor.YELLOW + "Homes de " + ChatColor.WHITE + "" + ChatColor.BOLD + player.getName(), sender);
+    @Default
+    @Description("List homes.")
+    public void homes(Player player){
+        this.openHomeList(player, player);
     }
+
+    @CatchUnknown
+    @Description("List homes of a player.")
+    @CommandPermission("xmas.homes.list.other")
+    @CommandCompletion("@target")
+    public void homes(Player player, OfflinePlayer target){
+        this.openHomeList(player, target);
+    }
+    @HelpCommand
+    public void onHelp(Player player){
+        player.sendMessage(Component.text(String.format("/homes%s", player.hasPermission("xmas.homes.list.other") ? " [joueur]" : "")).color(NamedTextColor.RED));
+    }
+
+    private void openHomeList(Player player, OfflinePlayer target){
+        var view = new HomeListView(this.core, 4 * 9, ChatColor.YELLOW + "Homes de " + ChatColor.WHITE + "" + ChatColor.BOLD + target.getName(),
+                this.core.getHomeManager().getHomes(target.getUniqueId()).stream().toList(), target);
+        var viewFrame = ViewFrame.of(this.core, view).register();
+        viewFrame.open(view.getClass(), player);
+    }
+
 }
