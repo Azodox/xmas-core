403cb3c771951b13b55e4d46e98807470973ec40
Azodox_ <likemoi99@gmail.com>
2022-08-08 04:19:26 +0200

    Using new event bus for account system

--- src/main/java/fr/olten/xmas/listener/RankChangedListener.java
+++ src/main/java/fr/olten/xmas/listener/RankChangedListener.java
@@ -1,21 +1,21 @@
 package fr.olten.xmas.listener;
 
 import fr.olten.xmas.manager.TeamNameTagManager;
-import net.valneas.account.api.events.rank.MajorRankChangedEvent;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import net.valneas.account.events.rank.MajorRankChangedEvent;
+import net.valneas.account.lib.eventbus.Subscribe;
+import org.bukkit.command.CommandSender;
 
 /**
  * Listener for major rank changes.
  */
-public class RankChangedListener implements Listener {
+public class RankChangedListener {
 
     /**
      * Handle the event when a player change his rank
      * @param event The event
      */
-    @EventHandler
-    public void onRankChange(MajorRankChangedEvent event) {
-        TeamNameTagManager.update(event.getAccount());
+    @Subscribe
+    public void onRankChange(MajorRankChangedEvent<CommandSender> event) {
+        TeamNameTagManager.update(event.getAccountManager());
     }
 }
